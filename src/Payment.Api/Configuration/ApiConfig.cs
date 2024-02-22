91a54f5be9b3534259be6aa7e30067f98b94db7a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-22 19:20:37 -0300

    ApiConfig class

---
+++ src/Payment.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Payment.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection WebApiConfig(this IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                });
+
+
+
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            services.AddCors(options =>
+                {
+                    options.AddPolicy("Development",
+                        builder => builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+
+                    options.AddPolicy("Production",
+                        builder =>
+                            builder
+                                .WithMethods("GET")
+                                .WithOrigins("http://google.com.br")
+                                .SetIsOriginAllowedToAllowWildcardSubdomains()
+                                //.WithHeaders(HeaderNames.ContentType, "x-custom-header")
+                                .AllowAnyHeader());
+                }
+
+            );
+
+
+            return services;
+        }
+    }
+}
