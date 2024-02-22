2ed7ae60299f1d70bcd4707a089fce2a2d9f4f36
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-22 19:13:26 -0300

    GuidValidation Attribute

---
+++ src/Payment.Api/Attributes/GuidValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Payment.Api.Attributes
+{
+    public class GuidValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+            {
+                // Treat null or empty strings as valid to allow for other validation attributes
+                return new ValidationResult(ErrorMessage ?? "Invalid GUID format.");
+            }
+
+            string? guidString = value?.ToString();
+
+
+            if (!Guid.TryParse(guidString, out _) || Guid.Empty.ToString() == guidString)
+            {
+                return new ValidationResult(ErrorMessage ?? "Invalid GUID format.");
+            }
+
+
+            return ValidationResult.Success;
+        }
+    }
+}
