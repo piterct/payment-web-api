0ce310f2471966ff635eec20464960a98a35004b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-22 19:38:20 -0300

    orderdto

---
+++ src/Payment.Business/Dtos/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Payment.Business.Dtos
+{
+    public record  OrderDto
+    {
+        public Guid OrderId { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+        public List<OrderItemDto> Items { get; set; }
+        public Seller Seller { get; set; }
+
+        public OrderDto() { }
+        public OrderDto(Guid orderId, string status, DateTime date, List<OrderItemDto> items, Seller seller)
+        {
+            OrderId = orderId;
+            Status = status;
+            Date = date;
+            Items = items;
+            Seller = seller;
+        }
+    }
+}
