120866b164897fe5e207757786367668ec071862
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-22 20:10:57 -0300

    Startup Extensions class

---
+++ src/Payment.Api/Extensions/StartupExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Payment.Api.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder webApplicationBuilder) where TStartup : IStartupProject
+        {
+            var startup = Activator.CreateInstance(typeof(TStartup), webApplicationBuilder.Configuration) as IStartupProject;
+            if (startup == null) throw new ArgumentException("Invalid Startup.cs class");
+
+
+            startup.ConfigureServices(webApplicationBuilder.Services);
+
+            var app = webApplicationBuilder.Build();
+
+            var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+            startup.Configure(app, app.Environment, provider);
+
+            app.Run();
+
+            return webApplicationBuilder;
+        }
+    }
+}
