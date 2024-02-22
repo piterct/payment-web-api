6f3d2a391ae55d60b721783b11fdf7f2d6e526c1
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-22 19:11:34 -0300

    startup class

---
+++ src/Payment.Api/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Payment.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment enviroment, IApiVersionDescriptionProvider provider)
+        {
+           
+        }
+    }
+
+
+    public interface IStartupProject
+    {
+        IConfiguration Configuration { get; }
+        void Configure(WebApplication app, IWebHostEnvironment enviroment, IApiVersionDescriptionProvider provider);
+        void ConfigureServices(IServiceCollection services);
+    }
+}
