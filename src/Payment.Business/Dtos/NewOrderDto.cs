1aeefdd3098053ab8455e3b96548c50927a34984
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-22 19:38:05 -0300

    new order Dto

---
+++ src/Payment.Business/Dtos/NewOrderDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Payment.Business.Dtos
+{
+    public class NewOrderDto
+    {
+        public NewOrderDto(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public NewOrderDto()
+        { }
+        public Guid OrderId { get; set; }
+    }
+}
