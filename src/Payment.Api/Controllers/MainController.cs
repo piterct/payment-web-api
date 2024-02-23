43283c9c474467e23bb66a4716eb2c63edde0cb4
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 13:37:28 -0300

    MainController class

--- src/Payment.Api/Controllers/MainController.cs
+++ src/Payment.Api/Controllers/MainController.cs
@@ -1,6 +1,70 @@
-﻿namespace Payment.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Payment.Business.Interfaces.Notifications;
+using Payment.Business.Notifications;
+
+namespace Payment.Api.Controllers
 {
-    public class MainController
+    public class MainController : ControllerBase
     {
+        private readonly INotifier _notifier;
+
+        public MainController(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        protected bool ValidOperation()
+        {
+            return !_notifier.HasNotification();
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotifyInvalidModelError(modelState);
+            return CustomResponse();
+        }
+
+        protected void NotifyInvalidModelError(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                var errorMessage = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+                NotifyError(errorMessage);
+            }
+        }
+
+        protected void NotifyError(string message)
+        {
+            _notifier.Handle(new Notification(message));
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                erros = _notifier.GetNotifications().Select(n => n.Message)
+            });
+        }
+
+        protected ActionResult CustomResponseNotFound(object result = null)
+        {
+            return NotFound(new
+            {
+                success = false,
+                erros = _notifier.GetNotifications().Select(n => n.Message)
+            });
+        }
     }
 }
