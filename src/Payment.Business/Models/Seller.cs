6f795c96a670f30420ab809c1950c3c2068f0588
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 00:31:56 -0300

    Seller model

---
+++ src/Payment.Business/Models/Seller.cs
@@ -0,0 +1,29 @@
+﻿namespace Payment.Business.Models
+{
+    public class Seller : Entity
+    {
+        public Seller(string cpf, string name, string email, string phone)
+        {
+            Cpf = cpf;
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        public Seller(Guid id, string cpf, string name, string email, string phone)
+        {
+            Id = id;
+            Cpf = cpf;
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        public Seller() { }
+
+        public string Cpf { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+    }
+}
