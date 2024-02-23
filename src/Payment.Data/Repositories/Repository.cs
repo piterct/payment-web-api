f99bb7b76173b52dce9b041d3de06d146e253189
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 20:21:03 -0300

    Repository  implementation

---
+++ src/Payment.Data/Repositories/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Payment.Business.Interfaces.Repositories;
+using Payment.Business.Models;
+using Payment.Data.Contexts;
+using System.Collections.Generic;
+
+namespace Payment.Data.Repositories
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly PaymentDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+    }
+}
