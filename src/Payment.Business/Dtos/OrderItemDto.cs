77546361ffca9517e78094d8b721ed9cc66f0f33
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 00:40:06 -0300

    OrderItem Dto

---
+++ src/Payment.Business/Dtos/OrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Payment.Business.Dtos
+{
+    public class OrderItemDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public OrderItemDto()
+        { }
+    }
+}
