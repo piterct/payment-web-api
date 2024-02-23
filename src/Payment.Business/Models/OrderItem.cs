dde99f1e6687db2a3b801fcf14b7c8316b4240a9
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 13:39:35 -0300

    Order Item Model

---
+++ src/Payment.Business/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Payment.Business.Models
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem(string name)
+        {
+            Name = name;
+        }
+
+        public OrderItem() { }
+        public Guid OrderId { get; private set; }
+        public string Name { get; private set; }
+
+        public Order Order { get; private set; }
+
+        public void AssociateOrder(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
