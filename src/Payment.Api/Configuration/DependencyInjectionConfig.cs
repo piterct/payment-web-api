d7cd26e65a824d1c2231c3ea855082e120c72a0d
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 00:30:40 -0300

    Dependency injection

---
+++ src/Payment.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Payment.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
