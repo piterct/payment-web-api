e37c4f74152ba9891dc8e41803b871228bec4f12
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 13:10:02 -0300

    Order Request view model

---
+++ src/Payment.Api/ViewModels/OrderRequest.cs
@@ -0,0 +1,19 @@
+﻿using Payment.Api.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Payment.Api.ViewModels
+{
+    public class OrderRequest
+    {
+        public OrderRequest(string sellerID, List<OrderItemRequest> items)
+        {
+            SellerId = sellerID;
+            Items = items;
+        }
+        [GuidValidation(ErrorMessage = "Invalid GUID format.")]
+        [Required(ErrorMessage = "The {0} field is mandatory")]
+        public string SellerId { get; set; }
+
+        public List<OrderItemRequest> Items { get; set; } = new List<OrderItemRequest>();
+    }
+}
