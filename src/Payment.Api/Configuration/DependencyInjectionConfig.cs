e945f808b975963b6c0529d0e8e641bbcec033d0
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 20:04:42 -0300

    Seller Service config

--- src/Payment.Api/Configuration/DependencyInjectionConfig.cs
+++ src/Payment.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,22 @@
-﻿namespace Payment.Api.Configuration
+﻿using Microsoft.Extensions.Options;
+using static Payment.Api.Configuration.SwaggerConfig.SwaggerDefaultValues;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Payment.Business.Interfaces.Services;
+using Payment.Business.Services;
+
+namespace Payment.Api.Configuration
 {
     public static class DependencyInjectionConfig
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
+            #region Configurations
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            #endregion
+
+            #region Services
+            services.AddScoped<ISellerService, SellerService>();
+            #endregion
 
             return services;
         }
