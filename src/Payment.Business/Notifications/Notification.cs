d860287ca35d9b83f8fdb94a76f7d68fc8184f60
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 13:09:44 -0300

    Notification class

---
+++ src/Payment.Business/Notifications/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace Payment.Business.Notifications
+{
+    public class Notification
+    {
+        public Notification(string message)
+        {
+            Message = message;
+        }
+        public string Message { get; set; }
+    }
+}
