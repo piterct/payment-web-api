4901abf3ab4a545ff5364163b6219d67f9550631
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-23 00:39:42 -0300

    INotifier

---
+++ src/Payment.Business/Interfaces/Notifications/INotifier.cs
@@ -0,0 +1,11 @@
+﻿using Payment.Business.Notifications;
+
+namespace Payment.Business.Interfaces.Notifications
+{
+    public interface  INotifier
+    {
+        bool HasNotification();
+        List<Notification> GetNotifications();
+        void Handle(Notification notification);
+    }
+}
