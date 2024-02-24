d0fb67b3aac3a771b17383c5214d050528e0071c
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 01:59:19 -0300

    IRepository

---
+++ src/Payment.Business/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Payment.Business.Models;
+using System.Linq.Expressions;
+
+namespace Payment.Business.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Add(TEntity entity);
+        Task<TEntity?> GetById(Guid id);
+        Task<List<TEntity>> GetAll();
+        Task Update(TEntity entity);
+        Task Remove(Guid id);
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
