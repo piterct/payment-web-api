58833a043769cd04bc0bc43296bf2c1bf7e64786
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 20:43:30 -0300

    OrderMapping entity

---
+++ src/Payment.Data/Mapping/OrderMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Payment.Business.Models;
+
+namespace Payment.Data.Mapping
+{
+    public class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.HasMany(c => c.OrderItems)
+                .WithOne(c => c.Order)
+                .HasForeignKey(c => c.OrderId);
+
+            builder.ToTable("Orders");
+        }
+    }
+}
