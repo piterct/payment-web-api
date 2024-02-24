2038f3a2531b5151e17070ec3e3a52e21e39830a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 01:59:35 -0300

    Notifier class

---
+++ src/Payment.Business/Notifications/Notifier.cs
@@ -0,0 +1,29 @@
+﻿using Payment.Business.Interfaces.Notifications;
+
+namespace Payment.Business.Notifications
+{
+    public class Notifier : INotifier
+    {
+        private readonly List<Notification> _notifications;
+
+        public Notifier()
+        {
+            _notifications = new List<Notification>();
+        }
+
+        public void Handle(Notification notification)
+        {
+            _notifications.Add(notification);
+        }
+
+        public List<Notification> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public bool HasNotification()
+        {
+            return _notifications.Any();
+        }
+    }
+}
