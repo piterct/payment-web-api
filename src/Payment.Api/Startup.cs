8b7e2a39f3b801d5f8cc0b1c03546e8b667d8965
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 03:07:08 -0300

    startup entity in memory

--- src/Payment.Api/Startup.cs
+++ src/Payment.Api/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Payment.Api.Configuration;
+using Payment.Data.Contexts;
 
 namespace Payment.Api
 {
@@ -13,12 +15,36 @@ namespace Payment.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-           
+            services.AddDbContext<PaymentDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("PaymentDb");
+            });
+
+
+            services.AddAutoMapper(typeof(Startup));
+            services.WebApiConfig();
+            services.AddSwaggerConfig();
+            services.ResolveDependencies();
         }
 
         public void Configure(WebApplication app, IWebHostEnvironment environment, IApiVersionDescriptionProvider provider)
         {
-           
+            if (environment.IsDevelopment())
+            {
+                app.UseCors("Development");
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseCors("Production");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwaggerConfig(provider);
+
+            app.MapControllers();
         }
     }
 
