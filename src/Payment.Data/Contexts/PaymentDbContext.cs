8c3cf77e86cb5d0c5a7f7eae8391e65485bea370
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 03:13:12 -0300

    PaymentDbContext

---
+++ src/Payment.Data/Contexts/PaymentDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Payment.Data.Contexts
+{
+    public class PaymentDbContext : DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                         .SelectMany(e => e.GetProperties()
+                             .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PaymentDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
