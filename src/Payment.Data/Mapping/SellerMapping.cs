cd5c76a603cee3cc24a9e8be419d46089f50ca54
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 03:07:44 -0300

    SellerMaping

---
+++ src/Payment.Data/Mapping/SellerMapping.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Payment.Business.Models;
+
+namespace Payment.Data.Mapping
+{
+    public  class SellerMapping : IEntityTypeConfiguration<Seller>
+    {
+        public void Configure(EntityTypeBuilder<Seller> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Cpf)
+                .IsRequired()
+                .HasColumnType("varchar(11)");
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(x => x.Phone)
+                .IsRequired()
+                .HasColumnType("varchar(15)");
+
+            builder.ToTable("Sellers");
+        }
+    }
+}
