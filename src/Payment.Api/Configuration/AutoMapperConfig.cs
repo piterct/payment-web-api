a86c45947b3ba14e9089b7d8b4cf9c5856ec1199
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 03:12:37 -0300

    AutoMapper Config

--- src/Payment.Api/Configuration/AutoMapperConfig.cs
+++ src/Payment.Api/Configuration/AutoMapperConfig.cs
@@ -1,6 +1,25 @@
-﻿namespace Payment.Api.Configuration
+﻿using AutoMapper;
+using Payment.Api.ViewModels;
+using Payment.Business.Dtos;
+using Payment.Business.Models;
+
+namespace Payment.Api.Configuration
 {
-    public class AutoMapperConfig
+    public class AutoMapperConfig : Profile
     {
+        public AutoMapperConfig()
+        {
+            #region Entities
+            CreateMap<Seller, SellerRequest>().ReverseMap();
+            CreateMap<OrderItem, OrderItemRequest>().ReverseMap();
+            CreateMap<OrderRequest, Order>()
+                .ForMember(dest => dest._orderItems, opt => opt.MapFrom(src => src.Items));
+
+            #endregion
+
+            #region Dtos
+            CreateMap<OrderItemDto, OrderItem>().ReverseMap();
+            #endregion 
+        }
     }
 }
