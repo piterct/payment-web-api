e9eedb5d092b6675c06f8d0b74012fc02bb0587b
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-24 03:07:29 -0300

    Base Service class

---
+++ src/Payment.Business/Services/BaseService.cs
@@ -0,0 +1,41 @@
+﻿using Payment.Business.Interfaces.Notifications;
+using Payment.Business.Models;
+using Payment.Business.Notifications;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Payment.Business.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotifier _notifier;
+
+        public BaseService(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        protected void Notify(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notify(error.ErrorMessage);
+            }
+        }
+
+        protected void Notify(string message)
+        {
+            _notifier.Handle(new Notification(message));
+        }
+
+        protected bool RunValidation<TV, TE>(TV validation, TE entity) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validation.Validate(entity);
+            if (validator.IsValid) return true;
+
+            Notify(validator);
+
+            return false;
+        }
+    }
+}
