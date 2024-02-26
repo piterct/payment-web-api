1bb47783d3ff255a94d78463bc1d2da205964d89
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-25 23:44:20 -0300

    number helper class

---
+++ src/Payment.Business/Helpers/NumberHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Payment.Business.Helpers
+{
+    public static class NumberHelper
+    {
+        public static string OnlyNumbers(string value)
+        {
+            var onlyNumber = "";
+            foreach (var s in value)
+            {
+                if (char.IsDigit(s))
+                {
+                    onlyNumber += s;
+                }
+            }
+            return onlyNumber.Trim();
+        }
+    }
+}
