7b1aa70cbd1fe5686174c78582280bb8f66b50e3
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-25 23:21:53 -0300

    order repository implementation

---
+++ src/Payment.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payment.Business.Interfaces.Repositories;
+using Payment.Business.Models;
+using Payment.Data.Contexts;
+
+namespace Payment.Data.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(PaymentDbContext context) : base(context) { }
+
+        public async Task<Order?> GetAllItemsById(Guid id)
+        {
+            return await Db.Orders.AsNoTracking()
+                .Include(o => o.OrderItems)
+                .FirstOrDefaultAsync(o => o.Id == id);
+        }
+    }
+}
