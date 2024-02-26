340168cb88ba12c97a805688b119c09d9a2b2a29
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:15:02 -0300

    Verification Digit Helper

---
+++ src/Payment.Business/Helpers/VerificationDigitHelper.cs
@@ -0,0 +1,61 @@
+﻿namespace Payment.Business.Helpers
+{
+    public class VerificationDigitHelper
+    {
+        private string _number;
+        private const int Module = 11;
+        private readonly List<int> _multipliers = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9 };
+        private readonly IDictionary<int, string> _replacements = new Dictionary<int, string>();
+        private bool _moduleComplement = true;
+
+        public VerificationDigitHelper(string number)
+        {
+            _number = number;
+        }
+
+        public VerificationDigitHelper WithMultipliersFromTo(int firstMultiplier, int lastMultiplier)
+        {
+            _multipliers.Clear();
+            for (var i = firstMultiplier; i <= lastMultiplier; i++)
+                _multipliers.Add(i);
+
+            return this;
+        }
+
+        public VerificationDigitHelper Substituting(string replacement, params int[] digits)
+        {
+            foreach (var i in digits)
+            {
+                _replacements[i] = replacement;
+            }
+            return this;
+        }
+
+        public void AddDigit(string digit)
+        {
+            _number = string.Concat(_number, digit);
+        }
+
+        public string CalculateDigit()
+        {
+            return !(_number.Length > 0) ? "" : GetDigitSum();
+        }
+
+        private string GetDigitSum()
+        {
+            var sum = 0;
+            for (int i = _number.Length - 1, m = 0; i >= 0; i--)
+            {
+                var product = (int)char.GetNumericValue(_number[i]) * _multipliers[m];
+                sum += product;
+
+                if (++m >= _multipliers.Count) m = 0;
+            }
+
+            var mod = (sum % Module);
+            var result = _moduleComplement ? Module - mod : mod;
+
+            return _replacements.ContainsKey(result) ? _replacements[result] : result.ToString();
+        }
+    }
+}
