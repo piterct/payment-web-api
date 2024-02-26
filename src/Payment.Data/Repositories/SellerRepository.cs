8207b5c321b757926640b360990feb6f40570fe8
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:56:39 -0300

    Seller Repository Implementation

---
+++ src/Payment.Data/Repositories/SellerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Payment.Business.Interfaces.Repositories;
+using Payment.Business.Models;
+using Payment.Data.Contexts;
+
+namespace Payment.Data.Repositories
+{
+    public class SellerRepository : Repository<Seller>, ISellerRepository
+    {
+        public SellerRepository(PaymentDbContext context) : base(context) { }
+    }
+}
