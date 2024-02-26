5591b064f742de3ab68ca2df702c88e443b30b98
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:56:19 -0300

    Repository Implementation

--- src/Payment.Data/Repositories/Repository.cs
+++ src/Payment.Data/Repositories/Repository.cs
@@ -1,7 +1,8 @@
-﻿using Payment.Business.Interfaces.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using Payment.Business.Interfaces.Repositories;
 using Payment.Business.Models;
 using Payment.Data.Contexts;
-using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Payment.Data.Repositories
 {
@@ -9,5 +10,54 @@ namespace Payment.Data.Repositories
     {
         protected readonly PaymentDbContext Db;
         protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository(PaymentDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<TEntity?> GetById(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntity>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+        public virtual async Task Add(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            Db.Entry(entity).State = EntityState.Modified;
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Remove(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
     }
 }
