31a7adaf74f0d5c38eed02689d295cc760b6ea04
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:38:35 -0300

    Order Service implementation

---
+++ src/Payment.Business/Queries/OrderQuery.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Payment.Business.Dtos;
+using Payment.Business.Interfaces.Queries;
+using Payment.Business.Interfaces.Repositories;
+
+namespace Payment.Business.Queries
+{
+    public class OrderQuery : IOrderQuery
+    {
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+        private readonly ISellerRepository _sellerRepository;
+
+        public OrderQuery(IMapper mapper, IOrderRepository orderRepository, ISellerRepository sellerRepository)
+        {
+            _mapper = mapper;
+            _orderRepository = orderRepository;
+            _sellerRepository = sellerRepository;
+        }
+        public async Task<OrderDto> GetById(Guid orderId)
+        {
+            var order = await _orderRepository.GetAllItemsById(orderId);
+            if (order is not null)
+            {
+                var seller = await _sellerRepository.GetById(order.SellerId);
+
+                var orderItemDto = _mapper.Map<List<OrderItemDto>>(order.OrderItems);
+
+                return new OrderDto(order.Id, order.Status.ToString(), order.Date, orderItemDto, seller);
+            }
+
+            return new OrderDto();
+        }
+    }
+}
