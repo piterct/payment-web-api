39fb40f74ec5ddb2dde02cd1715ea114d0495785
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:15:40 -0300

    IOrder Service Interface

---
+++ src/Payment.Business/Interfaces/Services/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using Payment.Business.Dtos;
+using Payment.Business.Enums;
+using Payment.Business.Models;
+
+namespace Payment.Business.Interfaces.Services
+{
+    public interface IOrderService
+    {
+        Task<NewOrderDto> Add(Order order);
+        Task<OrderDto> UpdateOrderStatus(Order order, EOrderStatus newStatus);
+    }
+}
