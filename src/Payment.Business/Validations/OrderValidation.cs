12abf7dd95a748a158fca1bafa638b8b3e29f778
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:51:43 -0300

    OrderValidation implementation

---
+++ src/Payment.Business/Validations/OrderValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Payment.Business.Models;
+
+namespace Payment.Business.Validations
+{
+    public class OrderValidation : AbstractValidator<Order>
+    {
+        public OrderValidation()
+        {
+            RuleFor(f => f.OrderItems.Count)
+                .NotEqual(0).WithMessage("A Order must have at least 1 item");
+
+            RuleFor(f => f.SellerId)
+                .NotEqual(Guid.Empty).WithMessage("It's necessary to have a seller for the order.");
+        }
+    }
+}
