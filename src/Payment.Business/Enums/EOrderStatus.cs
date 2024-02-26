bdb0231fdcea637a2aed372cad87d61d6d7f1c82
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-25 23:05:21 -0300

    EOrderStatus enum

---
+++ src/Payment.Business/Enums/EOrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Payment.Business.Enums
+{
+    public enum  EOrderStatus
+    {
+        AwaitingPayment = 0,
+        Approved = 1,
+        Shipped = 2,
+        Delivered = 3,
+        Cancelled = 4
+    }
+}
