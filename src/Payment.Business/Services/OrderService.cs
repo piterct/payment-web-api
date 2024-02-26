5746852ee8d3a9ef4a4656e1b55d003500d8e12a
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-02-26 00:39:04 -0300

    Services implementations

---
+++ src/Payment.Business/Services/OrderService.cs
@@ -0,0 +1,91 @@
+﻿using Payment.Business.Dtos;
+using Payment.Business.Enums;
+using Payment.Business.Interfaces.Notifications;
+using Payment.Business.Interfaces.Queries;
+using Payment.Business.Interfaces.Repositories;
+using Payment.Business.Interfaces.Services;
+using Payment.Business.Models;
+using Payment.Business.Validations;
+using static Payment.Business.Models.Order;
+
+namespace Payment.Business.Services
+{
+    public class OrderService : BaseService, IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderQuery _orderQuery;
+
+        public OrderService(INotifier notifier, IOrderRepository orderRepository, IOrderQuery orderQuery) : base(notifier)
+        {
+            _orderRepository = orderRepository;
+            _orderQuery = orderQuery;
+        }
+
+        public async Task<NewOrderDto> Add(Order order)
+        {
+            var newOrder = OrderFactory.NewOrder(order.SellerId);
+
+            newOrder.AddOrderItems(order._orderItems);
+
+            if (!RunValidation(new OrderValidation(), newOrder)) return new NewOrderDto();
+
+            await _orderRepository.Add(newOrder);
+
+            return new NewOrderDto(newOrder.Id);
+        }
+
+        public async Task<OrderDto> UpdateOrderStatus(Order order, EOrderStatus newStatus)
+        {
+            if (IsStatusUpdateAllowed(order.Status, newStatus))
+            {
+                order.UpdateOrderStatus(newStatus);
+                await _orderRepository.Update(order);
+                return await _orderQuery.GetById(order.Id);
+            }
+            else
+            {
+                Notify(string.Concat("It's not allowed to update the order status from ", order.Status.ToString(), " to ", newStatus.ToString()));
+                return new OrderDto();
+            }
+        }
+
+        private bool IsStatusUpdateAllowed(EOrderStatus currentStatus, EOrderStatus newStatus)
+        {
+            switch (currentStatus)
+            {
+                case EOrderStatus.AwaitingPayment:
+                    return IsStatusAwaitingPaymentUpdateAllowed(newStatus);
+                case EOrderStatus.Approved:
+                    return IsStatusApprovedUpdateAllowed(newStatus);
+                case EOrderStatus.Shipped:
+                    return IsStatusShippedAllowed(newStatus);
+                default:
+                    return false;
+            }
+        }
+
+        private bool IsStatusAwaitingPaymentUpdateAllowed(EOrderStatus newStatus)
+        {
+            if (newStatus == EOrderStatus.Approved || newStatus == EOrderStatus.Cancelled)
+                return true;
+
+            return false;
+        }
+
+        private bool IsStatusApprovedUpdateAllowed(EOrderStatus newStatus)
+        {
+            if (newStatus == EOrderStatus.Shipped || newStatus == EOrderStatus.Cancelled)
+                return true;
+
+            return false;
+        }
+
+        private bool IsStatusShippedAllowed(EOrderStatus newStatus)
+        {
+            if (newStatus == EOrderStatus.Delivered)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ src/Payment.Business/Services/SellerService.cs
@@ -0,0 +1,33 @@
+﻿using Payment.Business.Interfaces.Notifications;
+using Payment.Business.Interfaces.Repositories;
+using Payment.Business.Interfaces.Services;
+using Payment.Business.Models;
+using Payment.Business.Validations;
+
+namespace Payment.Business.Services
+{
+    public class SellerService : BaseService, ISellerService
+    {
+        private readonly ISellerRepository _sellerRepository;
+        public SellerService(INotifier notifier, ISellerRepository sellerRepository) : base(notifier)
+        {
+            _sellerRepository = sellerRepository;
+        }
+        public async Task<bool> Add(Seller seller)
+        {
+            if (!RunValidation(new SellerValidation(), seller)) return false;
+
+            var newSeller = await _sellerRepository.GetById(seller.Id);
+
+            if (newSeller is not null)
+            {
+                Notify("Seller with this ID already added.");
+                return false;
+            }
+
+            await _sellerRepository.Add(seller);
+
+            return true;
+        }
+    }
+}
