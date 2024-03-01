3510a3c879a066f1b760a8691be3dbeb5080be50
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-01 00:48:01 -0300

    implementation unitofwork

--- src/Payment.Data/Contexts/PaymentDbContext.cs
+++ src/Payment.Data/Contexts/PaymentDbContext.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Payment.Business.Interfaces.Data;
 using Payment.Business.Models;
 
 namespace Payment.Data.Contexts
 {
-    public class PaymentDbContext : DbContext
+    public class PaymentDbContext : DbContext, IUnitOfWork
     {
         public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options) { }
 
         public DbSet<Seller> Sellers { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("Date") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("Date").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("Date").IsModified = false;
+                }
+            }
+
+
+            return await base.SaveChangesAsync() > 0;
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
