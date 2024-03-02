0f0c3adac30a66aaddc1b1f3d9682ccaa8919f94
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-02 00:52:12 -0300

    GetOrderById Query Tests

---
+++ tests/Payment.Tests/Queries/Order/GetByIdOrderQueryTests.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using Moq.AutoMock;
+using Payment.Business.Interfaces.Repositories;
+using Payment.Business.Models;
+using Payment.Business.Queries;
+
+namespace Payment.Tests.Queries.Order
+{
+    public class GetByIdOrderQueryTests
+    {
+        private readonly AutoMocker _mocker;
+        private readonly OrderQuery _orderQuery;
+        private readonly Guid _orderId;
+
+        public GetByIdOrderQueryTests()
+        {
+            _mocker = new AutoMocker();
+            _orderQuery = _mocker.CreateInstance<OrderQuery>();
+            _orderId = Guid.NewGuid();
+        }
+
+        [Fact(DisplayName = "Get order by id")]
+        [Trait("Category", "Order Queries")]
+        public async void Order_GetOrderById_MustReturnAOrder()
+        {
+            // Arrange
+            var order = Business.Models.Order.OrderFactory.NewOrder(_orderId);
+            var orderItem = new OrderItem("Bike");
+            orderItem.AssociateOrder(order.Id);
+            _mocker.GetMock<IOrderRepository>().Setup(query => query.GetAllItemsById(_orderId)).ReturnsAsync(await Task.FromResult(order));
+
+            // Act
+            await _orderQuery.GetById(_orderId);
+
+            // Assert 
+            _mocker.GetMock<IOrderRepository>().Verify(r => r.GetAllItemsById(It.IsAny<Guid>()), Times.Once());
+            _mocker.GetMock<ISellerRepository>().Verify(r => r.GetById(It.IsAny<Guid>()), Times.Once());
+        }
+
+        [Fact(DisplayName = "Order not found by Id")]
+        [Trait("Category", "Order Queries")]
+        public async void Order_GetOrderById_MustNotReturnACompleteOrder()
+        {
+            // Act
+            await _orderQuery.GetById(_orderId);
+
+            // Assert 
+            _mocker.GetMock<IOrderRepository>().Verify(r => r.GetAllItemsById(It.IsAny<Guid>()), Times.Once());
+            _mocker.GetMock<ISellerRepository>().Verify(r => r.GetById(It.IsAny<Guid>()), Times.Never());
+        }
+
+    }
+
+}
