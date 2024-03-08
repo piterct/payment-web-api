de292931b698435c14d5ebc64d5d35275d04f04d
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-03-07 21:10:19 -0300

    validate updateOrderStatus

--- src/Payment.Business/Validations/OrderValidation.cs
+++ src/Payment.Business/Validations/OrderValidation.cs
@@ -1,5 +1,8 @@
-﻿using FluentValidation;
+﻿using System.Data.SqlTypes;
+using FluentValidation;
+using Payment.Business.Enums;
 using Payment.Business.Models;
+using Payment.Business.Validations.Documents;
 
 namespace Payment.Business.Validations
 {
@@ -12,6 +15,15 @@ namespace Payment.Business.Validations
 
             RuleFor(f => f.SellerId)
                 .NotEqual(Guid.Empty).WithMessage("It's necessary to have a seller for the order.");
+        }
+
+        public OrderValidation UpdateStatusOrder(EOrderStatus eOrderStatus)
+        {
+            RuleFor(f => f.IsStatusUpdateAllowed(eOrderStatus)).Equal(true)
+                .WithMessage(x =>
+                    string.Concat("It's not allowed to update the order status from ", x.Status.ToString(), " to ", eOrderStatus.ToString()));
+
+            return this;
         }
     }
 }
