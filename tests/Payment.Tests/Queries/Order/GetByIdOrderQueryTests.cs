f0f08cc19c7bdd6e886f98b1bfffa07700dbb8c2
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-08-17 21:59:40 -0300

    assert the callback was filled with the same orderid was created using callback

--- tests/Payment.Tests/Queries/Order/GetByIdOrderQueryTests.cs
+++ tests/Payment.Tests/Queries/Order/GetByIdOrderQueryTests.cs
@@ -26,15 +26,22 @@ namespace Payment.Tests.Queries.Order
             // Arrange
             var order = Business.Models.Order.OrderFactory.NewOrder(_orderId);
             var orderItem = new OrderItem("Bike");
+            Guid orderIdCalled = Guid.Empty;
             orderItem.AssociateOrder(order.Id);
-            _mocker.GetMock<IOrderRepository>().Setup(query => query.GetAllItemsById(_orderId)).ReturnsAsync(await Task.FromResult(order));
+            _mocker.GetMock<IOrderRepository>().Setup(query => query.GetAllItemsById(_orderId))
+                .Callback<Guid>((orderId) =>
+                {
+                    orderIdCalled = orderId;
+                })
+                .ReturnsAsync(await Task.FromResult(order));
 
             // Act
-            await _orderQuery.GetById(_orderId);
+            var newOrder = await _orderQuery.GetById(_orderId);
 
             // Assert 
             _mocker.GetMock<IOrderRepository>().Verify(r => r.GetAllItemsById(It.IsAny<Guid>()), Times.Once());
             _mocker.GetMock<ISellerRepository>().Verify(r => r.GetById(It.IsAny<Guid>()), Times.Once());
+            Assert.Equal(_orderId, orderIdCalled);
         }
 
         [Fact(DisplayName = "Order not found by Id")]
