a44b687dddd0356d2da111c5d26f08f57eef2e1e
Michael Peter <42197306+piterct@users.noreply.github.com>
2024-11-17 16:53:16 -0300

    Option to nullable property

--- src/Payment.Api/ViewModels/OrderItemRequest.cs
+++ src/Payment.Api/ViewModels/OrderItemRequest.cs
@@ -6,6 +6,6 @@ namespace Payment.Api.ViewModels
     {
         [Required(ErrorMessage = "The {0} field is mandatory")]
         [StringLength(60, ErrorMessage = "The field {0} must have between {2} and {1} characters", MinimumLength = 3)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
